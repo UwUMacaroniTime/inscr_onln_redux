4508ad95c1cef24ddf865d6eb8413071b4f92fcf
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2024-06-23 13:09:07 -0400

    del junk

--- scenes/overworld/locations/transition_trigger.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-@export_file("*.tscn") var location:String 
-@export var position_group:StringName
-
-func _on_body_entered(body):
-	if body is Player:
-		get_tree().call_group(&"overworld", &"transition", load(location), position_group)
